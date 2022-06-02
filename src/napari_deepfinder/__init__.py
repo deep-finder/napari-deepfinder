47559ab07abe0f7cc217c8cbc473989b516417af
caronsso <constantin.aronssohn@inria.fr>
2022-06-02 14:37:57 +0200

    small fixes

--- src/napari_deepfinder/__init__.py
+++ src/napari_deepfinder/__init__.py
@@ -1,12 +1,21 @@
-
 __version__ = "0.0.1"
 
 
-from ._reader import napari_get_reader
-from ._writer import write_annotations_xml
-from ._widget import denoise_widget, reorder_widget, AddPointsWidget
-from ._orthoview_widget import Orthoslice
-
 # TODO: resolve the bugs that are causing the FutureWarnings
 import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from ._orthoview_widget import Orthoslice
+from ._reader import napari_get_reader
+from ._widget import AddPointsWidget, denoise_widget, reorder_widget
+from ._writer import write_annotations_xml
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+__all__ = (
+    "napari_get_reader",
+    "write_annotations_xml",
+    "denoise_widget",
+    "reorder_widget",
+    "AddPointsWidget",
+    "Orthoslice",
+)
--- src/napari_deepfinder/_tests/test_widget.py
+++ src/napari_deepfinder/_tests/test_widget.py
@@ -1,5 +1,11 @@
-from napari_deepfinder import Orthoslice
 import numpy as np
+
+from napari_deepfinder import (
+    AddPointsWidget,
+    ClusterWidget,
+    Orthoslice,
+    SegmentationWidget,
+)
 
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
@@ -13,16 +19,46 @@ def test_orthoslice(make_napari_viewer, qtbot):
     my_widget.checkbox.setChecked(True)
     my_widget._on_click_checkbox(True)
     # Check the viewfinders of the main viewer
-    assert my_widget.main_view.layers[-1].name == 'viewfinder_xy_y'
-    assert my_widget.main_view.layers[-2].name == 'viewfinder_xy_x'
+    assert my_widget.main_view.layers[-1].name == "viewfinder_xy_y"
+    assert my_widget.main_view.layers[-2].name == "viewfinder_xy_x"
     # Check the viewfinders of the xz viewer
-    assert my_widget.xz_view.layers[-1].name == 'viewfinder_xz_z'
-    assert my_widget.xz_view.layers[-2].name == 'viewfinder_xz_x'
+    assert my_widget.xz_view.layers[-1].name == "viewfinder_xz_z"
+    assert my_widget.xz_view.layers[-2].name == "viewfinder_xz_x"
     # Check the viewfinders of the yz viewer
-    assert my_widget.yz_view.layers[-1].name == 'viewfinder_yz_z'
-    assert my_widget.yz_view.layers[-2].name == 'viewfinder_yz_y'
+    assert my_widget.yz_view.layers[-1].name == "viewfinder_yz_z"
+    assert my_widget.yz_view.layers[-2].name == "viewfinder_yz_y"
     my_widget.checkbox.setChecked(False)
     my_widget._on_click_checkbox(False)
     my_widget.deleteLater()
     qtbot.wait(10)
-    pass
+
+
+def test_segmentation(make_napari_viewer, qtbot):
+    viewer = make_napari_viewer()
+    my_widget = SegmentationWidget(viewer)
+    assert int(my_widget.nb_classes.text()) == 13
+    my_widget.nb_classes.setValue(15)
+    assert int(my_widget.nb_classes.text()) == 15
+    my_widget.deleteLater()
+    qtbot.wait(10)
+
+
+def test_clustering(make_napari_viewer, qtbot):
+    viewer = make_napari_viewer()
+    my_widget = ClusterWidget(viewer)
+    assert int(my_widget.cluster_radius.text()) == 5
+    my_widget.cluster_radius.setValue(10)
+    assert int(my_widget.cluster_radius.text()) == 10
+    my_widget.deleteLater()
+    qtbot.wait(10)
+
+
+def test_add_points(make_napari_viewer, qtbot):
+    viewer = make_napari_viewer()
+    my_widget = AddPointsWidget(viewer)
+    viewer.add_points(data=None, ndim=3, name="test_layer")
+    my_widget._input_layer_box.setCurrentText("test_layer")
+    my_widget._add_point.toggle()
+    assert viewer.layers["test_layer"].data != np.array([])
+    my_widget.deleteLater()
+    qtbot.wait(10)
