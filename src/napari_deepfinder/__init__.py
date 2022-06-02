db9db1c372c9aec67b396568d47791996d955657
caronsso <constantin.aronssohn@inria.fr>
2022-06-02 15:06:56 +0200

    fix init

--- src/napari_deepfinder/__init__.py
+++ src/napari_deepfinder/__init__.py
@@ -4,8 +4,10 @@ __version__ = "0.0.1"
 # TODO: resolve the bugs that are causing the FutureWarnings
 import warnings
 
+from ._cluster_widget import ClusterWidget
 from ._orthoview_widget import Orthoslice
 from ._reader import napari_get_reader
+from ._segmentation_widget import SegmentationWidget
 from ._widget import AddPointsWidget, denoise_widget, reorder_widget
 from ._writer import write_annotations_xml
 
@@ -18,4 +20,6 @@ __all__ = (
     "reorder_widget",
     "AddPointsWidget",
     "Orthoslice",
+    "SegmentationWidget",
+    "ClusterWidget",
 )
