d4600319abf02fbf879cbbce46390c74bdf5d17d
caronsso <constantin.aronssohn@inria.fr>
2022-06-03 14:47:53 +0200

    Fix test writer (one test was not executed)

--- src/napari_deepfinder/_tests/test_writer.py
+++ src/napari_deepfinder/_tests/test_writer.py
@@ -1,5 +1,4 @@
 from napari_deepfinder import write_annotations_xml
-from napari.layers import Points
 import numpy as np
 import os
 
@@ -10,7 +9,7 @@ def test_writing_one_layer(tmp_path):
     write_annotations_xml(path, [(data, {'name': 'test'}, 'points')])
 
 
-def test_writing_one_layer(tmp_path):
+def test_writing_multiple_layers(tmp_path):
     data = np.array([[0, 0, 0]])
     data_2 = np.array([[1, 1, 1], [0, 0, 0]])
     path = os.path.join(str(tmp_path), "test_labels")
