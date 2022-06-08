8be359d680fff90228332c2c1dd64c5f0dbf2183
caronsso <constantin.aronssohn@inria.fr>
2022-06-08 10:24:12 +0200

    Remove warning filters not needed anymore

--- src/napari_deepfinder/__init__.py
+++ src/napari_deepfinder/__init__.py
@@ -1,9 +1,5 @@
 __version__ = "0.0.1"
 
-
-# TODO: resolve the bugs that are causing the FutureWarnings
-import warnings
-
 from ._cluster_widget import ClusterWidget
 from ._orthoview_widget import Orthoslice
 from ._reader import napari_get_reader
@@ -11,7 +7,6 @@ from ._segmentation_widget import SegmentationWidget
 from ._widget import AddPointsWidget, denoise_widget, reorder_widget
 from ._writer import write_annotations_xml
 
-warnings.simplefilter(action="ignore", category=FutureWarning)
 
 __all__ = (
     "napari_get_reader",
