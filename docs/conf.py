099fd177c2e397dbabd564b4b94bba14ab83f3e5
caronsso <constantin.aronssohn@inria.fr>
2022-07-01 19:57:04 +0200

    Fix napari_deepfinder path in documentation config

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import shlex
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../napari_deepfinder'))
+sys.path.insert(0, os.path.abspath('../src/napari_deepfinder'))
 
 # -- General configuration ------------------------------------------------
 
