ef8b2476f165f65b893affb148464e391da9496d
caronsso <constantin.aronssohn@inria.fr>
2022-07-06 15:55:34 +0200

    Improve documentation

--- src/napari_deepfinder/_widget.py
+++ src/napari_deepfinder/_widget.py
@@ -16,6 +16,7 @@ def denoise_widget(
         activate_denoise=False,
 ):
     """
+    denoise_widget()
     Widget to denoise an image layer.
     When activated adds the denoised image layer on top of the old one and hides the old layer.
 
@@ -87,7 +88,9 @@ def denoise_widget(
 
 @magic_factory(auto_call=True, call_button="Reorder layers")
 def reorder_widget(viewer: 'napari.viewer.Viewer'):
-    """Widget to reorder the layers respecting the following order from bottom to top:
+    """
+    reorder_widget()
+    Widget to reorder the layers respecting the following order from bottom to top:
         - Image
         - Labels
         - Points
